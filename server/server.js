23cd3bccd8e9e33216d89b2620f801519d6cd2e9
kdaviesnz <kdavies@gmail.com>
2020-10-27 18:49:57 +1300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,13 +4,25 @@ import config from './../config/config'
 import app from './express'
 
 
-
-
-
-app.listen(config.port, (err) => {
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert');
+require("dotenv").config()
+// Database Connection URL
+const uri = "mongodb+srv://" + process.env.MONGODBUSER + ":" + process.env.MONGODBPASSWORD + "@cluster0.awqh6.mongodb.net/gamifier?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoClient.connect(err => {
+    assert.equal(err, null);
+    const collections = mongoClient.db('gamifier')
+    console.log("Connected successfully to mongodb server")  
+    
+    app.listen(config.port, (err) => {
     if (err) {
         console.log(err)
     }
     console.info('Server started on port %s.', config.port)
+    })
 })
+
+
+
 
