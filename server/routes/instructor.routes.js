2588eefaf21368afe5bc57607103e2dc98b81cd3
kdaviesnz <kdavies@gmail.com>
2020-10-27 18:39:06 +1300

    Update instructor.routes.js

--- server/routes/instructor.routes.js
+++ server/routes/instructor.routes.js
@@ -6,20 +6,10 @@ import express from 'express'
 import instructorCtrl from '../controllers/instructor.controller'
 import instructorAuthCtrl from '../controllers/instructor.auth.controller'
 
-const router = express.Router()
-
-const MongoClient = require('mongodb').MongoClient
-const assert = require('assert');
-require("dotenv").config()
-
-// Database Connection URL
-const uri = "mongodb+srv://" + process.env.MONGODBUSER + ":" + process.env.MONGODBPASSWORD + "@cluster0.awqh6.mongodb.net/gamifier?retryWrites=true&w=majority";
-const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-mongoClient.connect(err => {
-    assert.equal(err, null);
-    const collections = mongoClient.db('gamifier')
-    console.log("Connected successfully to mongodb server")
+const InstructorRouter = (collections) => {
+    
+    const router = express.Router()
+    
     router.route('/api/instructors')
         .get((req, res)=> {
             res.json([{
@@ -43,7 +33,7 @@ mongoClient.connect(err => {
                 })
             }
         )
-
+    
     /*
     router.route('/api/instructors/:userId')
         .get(instructorAuthCtrl.requireSignin, instructorCtrl.read)
@@ -52,9 +42,28 @@ mongoClient.connect(err => {
 
     router.param('userId', instructorAuthCtrl.userByID)
     */
+    
+    return router
 })
 
+module.exports = InstructorRouter
+
+/*
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert');
+require("dotenv").config()
+
+// Database Connection URL
+const uri = "mongodb+srv://" + process.env.MONGODBUSER + ":" + process.env.MONGODBPASSWORD + "@cluster0.awqh6.mongodb.net/gamifier?retryWrites=true&w=majority";
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoClient.connect(err => {
+    assert.equal(err, null);
+    const collections = mongoClient.db('gamifier')
+    console.log("Connected successfully to mongodb server")   
+})
+*/
 
 
-export default router
+
 
