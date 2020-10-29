5630835e3e6432826ac66d7a89bd7d35f4724c54
kdaviesnz <kdavies@gmail.com>
2020-10-29 13:19:05 +1300

    Update Course.js

--- client/user/Course.js
+++ client/user/Course.js
@@ -1,5 +1,9 @@
-module.exports = function Course() {
+const Course = () => {
   return <div>
     <a href="http://Node.University" target="_blank">Node.University</a> is home to top-notch Node education which brings joy to JavaScript engineers.
   </div>
 }
+
+export default Course
+
+
