4bdeaf5975d106dc6eb0c5d085ca543f1c8e6874
Kevin Davies <kevin@verum.nz>
2020-11-09 14:22:19 +1300

    ...

--- client/user/instructor/Signup.js
+++ client/user/instructor/Signup.js
@@ -14,8 +14,10 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import {Link} from 'react-router-dom'
-
-
+import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import SignupForm from "../../user/instructor/SignupForm"
 
 class Signup extends Component {
 
@@ -65,42 +67,14 @@ class Signup extends Component {
     }
 
     render() {
-        const {classes} = this.props
-        return (<div>
-            <Card className={classes.card}>
-                <CardContent>
-                    <Typography type="headline" component="h2" className={classes.title}>
-                        Instructor Sign Up
-                    </Typography>
-                    <TextField id="name" label="Name" className={classes.textField} value={this.state.name} onChange={this.handleChange('name')} margin="normal"/><br/>
-                    <TextField id="email" type="email" label="Email" className={classes.textField} value={this.state.email} onChange={this.handleChange('email')} margin="normal"/><br/>
-                    <TextField id="password" type="password" label="Password" className={classes.textField} value={this.state.password} onChange={this.handleChange('password')} margin="normal"/>
-                    <br/> {
-                    this.state.error && (<Typography component="p" color="error">
-                        <Icon color="error" className={classes.error}>error</Icon>
-                        {this.state.error}</Typography>)
-                }
-                </CardContent>
-                <CardActions>
-                    <Button color="primary" variant="outlined" onClick={this.clickSubmit} className={classes.submit}>Submit</Button>
-                </CardActions>
-            </Card>
-            <Dialog open={this.state.open} disableBackdropClick={true}>
-                <DialogTitle>New Account</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        New account successfully created.
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Link to="/instructorsignin">
-                        <Button color="primary" autoFocus="autoFocus" variant="outlined">
-                            Sign In
-                        </Button>
-                    </Link>
-                </DialogActions>
-            </Dialog>
-        </div>)
+        return <React.Fragment>
+            <CssBaseline />
+            <Container fixed>
+                <Box>
+                    <SignupForm open_dialog={this.state.open} error={this.state.error} clickSubmit={this.clickSubmit} user_name={this.state.name} email={this.state.email} handleChange={this.handleChange} password={this.state.password}/>
+                </Box>
+            </Container>
+        </React.Fragment>
     }
 }
 
---
+++ client/user/instructor/SignupForm.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+import Typography from "@material-ui/core/Typography/Typography";
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Icon from "@material-ui/core/Icon";
+import DialogTitle from "@material-ui/core/DialogTitle/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions/DialogActions";
+import {Link} from "react-router-dom";
+import Dialog from "@material-ui/core/Dialog/Dialog";
+
+const SignupForm = (props) => {
+
+    return <div><Card>
+        <CardContent>
+            <Typography type="headline" component="h2">
+                Instructor Sign Up
+            </Typography>
+            <TextField id="name" label="Name" value={props.user_name} onChange={this.handleChange('name')} margin="normal"/><br/>
+            <TextField id="email" type="email" label="Email" value={props.email} onChange={this.handleChange('email')} margin="normal"/><br/>
+            <TextField id="password" type="password" label="Password" value={props.password} onChange={this.handleChange('password')} margin="normal"/>
+            <br/> {
+                props.error && (<Typography component="p" color="error">
+                    <Icon color="error">error</Icon>
+                    {props.error}</Typography>)
+            }
+        </CardContent>
+        <CardActions>
+            <Button color="primary" variant="outlined" onClick={props.clickSubmit}>Submit</Button>
+        </CardActions>
+
+
+    </Card>
+
+        <Dialog open={props.open_dialog} disableBackdropClick={true}>
+            <DialogTitle>New Account</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    New account successfully created.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Link to="/instructor/signin">
+                    <Button color="primary" autoFocus="autoFocus" variant="outlined">
+                        Sign In
+                    </Button>
+                </Link>
+            </DialogActions>
+        </Dialog>
+
+    </div>
+
+}
+
+export default SignupForm
+
+
